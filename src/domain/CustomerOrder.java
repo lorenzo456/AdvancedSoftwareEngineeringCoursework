71c47b911b10381b10af27c04005ccaafda7fbbe
JowitaKK <jowita.knap@wp.pl>
2019-03-13 22:36:02 +0000

    move to logger to utils

--- src/domain/CustomerOrder.java
+++ src/domain/CustomerOrder.java
@@ -1,5 +1,7 @@
 package domain;
 
+import utils.Logger;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
--- src/domain/CustomerOrderProcessor.java
+++ src/domain/CustomerOrderProcessor.java
@@ -1,5 +1,7 @@
 package domain;
 
+import utils.Logger;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.FileSystems;
